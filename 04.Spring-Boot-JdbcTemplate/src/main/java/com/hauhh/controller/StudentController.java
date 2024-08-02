aac528f5120a3acca5cbb80e62824cb5ef91a32f
Hoang Hai Hau <hhhau1910@gmail.com>
2024-08-02 11:34:50 +0700

    add new controller: StudentController

---
+++ 04.Spring-Boot-JdbcTemplate/src/main/java/com/hauhh/controller/StudentController.java
@@ -0,0 +1,45 @@
+package com.hauhh.controller;
+
+import com.hauhh.entity.Student;
+import com.hauhh.service.StudentService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @RequestMapping(value = "/queryStudent", method = RequestMethod.GET)
+    public Student getStudentBySNO(String sno) {
+        return studentService.getStudentBySNO(sno);
+    }
+
+    @RequestMapping(value = "/getAllStudent", method = RequestMethod.GET)
+    public List<Map<String, Object>> queryAllStudent() {
+        return this.studentService.getAllStudent();
+    }
+
+    @RequestMapping(value = "/addStudent", method = RequestMethod.POST)
+    public int saveStudent(String sno,String name,String sex) {
+        Student student = new Student();
+        student.setSno(sno);
+        student.setName(name);
+        student.setSex(sex);
+        return this.studentService.addStudent(student);
+    }
+
+    @RequestMapping(value = "/deleteStudent", method = RequestMethod.DELETE)
+    public int deleteStudentBySno(String sno) {
+        return this.studentService.deleteStudent(sno);
+    }
+
+}
