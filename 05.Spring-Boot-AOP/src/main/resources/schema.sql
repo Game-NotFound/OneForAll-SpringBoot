b883742eacc7452475b4032a47a680619b125bbc
Hoang Hai Hau <hhhau1910@gmail.com>
2024-08-03 11:32:48 +0700

    adjust schema.sql

--- 05.Spring-Boot-AOP/src/main/resources/schema.sql
+++ 05.Spring-Boot-AOP/src/main/resources/schema.sql
@@ -1,20 +1,12 @@
 CREATE TABLE IF NOT EXISTS sys_log
 (
-    id          BIGINT       NOT NULL AUTO_INCREMENT,
-    username    VARCHAR(50)  NULL,
-    operation   VARCHAR(50)  NULL,
-    time        INT          NULL,
-    method      VARCHAR(200) NULL,
-    params      VARCHAR(500) NULL,
-    ip          VARCHAR(64)  NULL,
-    create_time DATETIME     NULL,
+    id          BIGINT       NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
+    username    VARCHAR(50)  NULL COMMENT 'User name',
+    operation   VARCHAR(50)  NULL COMMENT 'User operation',
+    time        INT          NULL COMMENT 'Response time',
+    method      VARCHAR(200) NULL COMMENT 'Request method',
+    params      VARCHAR(500) NULL COMMENT 'Request parameters',
+    ip          VARCHAR(64)  NULL COMMENT 'IP address',
+    create_time DATETIME     NULL COMMENT 'Creation time',
     PRIMARY KEY (id)
 );
-COMMENT ON COLUMN SYS_LOG.USERNAME IS 'User name';
-COMMENT ON COLUMN SYS_LOG.OPERATION IS 'User operation';
-COMMENT ON COLUMN SYS_LOG.TIME IS 'Response time';
-COMMENT ON COLUMN SYS_LOG.METHOD IS 'Request method';
-COMMENT ON COLUMN SYS_LOG.PARAMS IS 'Request parameters';
-COMMENT ON COLUMN SYS_LOG.IP IS 'IP address';
-COMMENT ON COLUMN SYS_LOG.CREATE_TIME IS 'Creation time';
-CREATE SEQUENCE seq_sys_log START WITH 1 INCREMENT BY 1;
