db8bdc946c3a452ed571443e61ea98d4efd11698
Hoang Hai Hau <hhhau1910@gmail.com>
2024-08-03 11:27:19 +0700

    create util HttpContext and IP

---
+++ 05.Spring-Boot-AOP/src/main/java/com/hauhh/util/HttpContextUtil.java
@@ -0,0 +1,16 @@
+package com.hauhh.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Objects;
+
+
+public class HttpContextUtil {
+
+    public static HttpServletRequest getHttpServletRequest() {
+        return ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
+    }
+
+}
---
+++ 05.Spring-Boot-AOP/src/main/java/com/hauhh/util/IPUtils.java
@@ -0,0 +1,20 @@
+package com.hauhh.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class IPUtils {
+
+    public static String getIPAddress(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return "0:0:0:0:0:0:0:1".equals(ip) ? "127.0.0.1" : ip;
+    }
+}
